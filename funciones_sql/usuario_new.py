50411ffcbb333eb1c236cd72db4d338e70e1a2be
Camilo <camilomunozb203@gmail.com>
2024-11-25 22:41:36 -0500

    Comentarios en el index y usuario_new.

--- funciones_sql/usuario_new.py
+++ funciones_sql/usuario_new.py
@@ -1,14 +1,41 @@
+
+# SE IMPORTA LA LIBRERIA "sqlite3" PARA EL MANEJO DE LA BASE DE DATOS.
+
 import sqlite3
+
+
+#  FUNCION QUE SERA USADA EN EL INDEX.
 
 def ingreso_usuario():
     try:
+
+        # CONEXION CON LA BASE DE DATOS.
+
         with sqlite3.connect("C:/Users/POWER/gastos_control.db") as nuevo_usuario:
+
+            # SE CREA UN CURSOR PARA MANEJAR LA BASE DE DATOS.
+
             consulta_cursor = nuevo_usuario.cursor()
+
+            # INGRESO DE USUARIO PARA INGRESAR SU NOMBRE Y APELLIDO.
+
             usuario_nuevo = str(input("Ingresa tu nombre: "))
             apellido_usuario = str(input("Ingresa tu apellido: "))
+
+            # CONSULTA SQL QUE INGRESA EL NOMBRE Y APELLIDO A LA BASE DE DATOS.
+
             consulta_cursor.execute("INSERT INTO usuario (nombre_usuario, apellido_usuario) VALUES (?,?)",(usuario_nuevo,apellido_usuario))
+
+            # SE SUBEN LOS CAMBIOS A LA BASE DE DATOS.
+
             nuevo_usuario.commit()
+
+            # MENSAJE DE EXITO.
+
             print("Ingreso exitoso.")
+
+    # MANEJO DE ERRORES.
+    
     except ValueError:
         print("Error de digitacion, volver a ingresar.")
     except sqlite3.Error as error:
--- index.py
+++ index.py
@@ -1,9 +1,16 @@
+
+# FUNCIONES IMPORTADAS AL INDEX PARA SU USO.
+
 from funciones_sql.usuario_new import ingreso_usuario
 from funciones_sql.gastos_usuario import gastos_ingresados
 from funciones_sql.eliminar_gastos import eliminar_gastos
 from funciones_sql.datos_completos import mostrar_datos
 
+
 while True:
+
+    # MENU DEL INDEX PARA EL USUARIO.
+
     print(
         """
             Bienvenido al control de gastos, elije una opcion:
@@ -14,8 +21,15 @@ while True:
             5. Salir
         """
     )
+
     try:
+        
+        # INGRESO DE USUARIO PARA INGRESAR UNA OPCION.
+
         usuario = int(input("Ingresa una opcion: "))
+
+        # CONDICIONALES CON LA FUNCION IMPORTADA PARA USARSE.
+
         if usuario == 1:
             ingreso_usuario()
         elif usuario == 2:
@@ -25,7 +39,13 @@ while True:
         elif usuario == 4:
             mostrar_datos()
         else:
+
+            # SALIDA DEL PROGRAMA.
+
             print("Gracias por usar el controlador de gastos, cuida tus ganancias.")
             break
+
+    # MANEJO DE ERROES.
+    
     except ValueError:
         print("Error de digitacion, volver a intentar")

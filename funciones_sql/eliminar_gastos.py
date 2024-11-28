cf8e74dd50f8e1604b113b592bc6341502eb7798
Camilo <camilomunozb203@gmail.com>
2024-11-27 22:29:56 -0500

    Inicio de comentatrios al proyecto.

--- funciones_sql/eliminar_gastos.py
+++ funciones_sql/eliminar_gastos.py
@@ -1,22 +1,56 @@
+
+# SE IMPORTA LA LIBRERIA "sqlite3" PARA EL MANEJO DE LA BASE DE DATOS.
+
 import sqlite3
 
 
+# FUNCION QUE SERA USADA EN EL INDEX.
+
 def eliminar_gastos():
     try:
+
+        # CONEXION CON LA BASE DE DATOS.
+
         with sqlite3.connect("C:/Users/POWER/gastos_control.db") as gastos_eliminar:
+
+            # SE CREA UN CURSOR PARA MANEJAR LA BASE DE DATOS.
+
             consulta_cursor = gastos_eliminar.cursor()
+
+            # ENTRADAS DE USUARIO QUE PIDE EL GASTO Y EL NOMBRE DEK USUARIO PARA ELIMINAR REGISTRO.
+
             gasto_eliminar = str(input("Ingresa el gasto a eliminar: "))
             nombre_usuario = str(input("Ingresa el nombre de registro: "))
             apellido_usuario = str(input("Ingresa el apellido del usuario registrado: "))
+
+            # CONSULTA SQL PARA IDENTIFICAR EL ID DEL USUARIO Y CON ESTE SU NOMBRE Y APELLIDO.
+
             consulta_cursor.execute("SELECT usuario_ID FROM usuario WHERE nombre_usuario = ? AND apellido_usuario = ?",(nombre_usuario,apellido_usuario))
+
+            # CON EL METODO "fetchone" SE BUSCA EL REGISTRO DE LA CLAVE.
+
             usuario = consulta_cursor.fetchone()
+
+
             if usuario:
+
+                #  RASTREAR AL CLIENTE EN LA BASE DE DATOS
+
                 usuario_id = usuario[0]
             else:
                 print("Usuario no encontrado.")
+
+            # CONSULTA SQL PARA ELIMINAR EL GASTO DEL USUARIO Y SU NOMBRE.
+
             consulta_cursor.execute("DELETE FROM gastos WHERE gasto_usuario = ? AND usuario_ID = ?",(gasto_eliminar,usuario_id))
+
+            # SE SUBEN LOS CAMBIOS A LA BASE DE DATOD.
+
             gastos_eliminar.commit()
             print("Gasto eliminado.")
+    
+    # MANWJO DE ERRORES.
+    
     except ValueError:
         print("Error de digitacion, volver a intentar.")
     except sqlite3.Error as error:
--- funciones_sql/gastos_usuario.py
+++ funciones_sql/gastos_usuario.py
@@ -1,21 +1,58 @@
+
+# SE IMPORTA LA LIBRERIA "sqlite3" PARA EL MANEJO DE LA BASE DE DATOS. 
+
 import sqlite3
+
+
+#  FUNCION QUE SERA USADA EN EL INDEX.
 
 def gastos_ingresados():
     try:
+
+        # CONEXION CON LA BASE DE DATOS.
+
         with sqlite3.connect("C:/Users/POWER/gastos_control.db") as ingresar_gastos:
+
+            # SE CREA UN CURSOR PARA MANEJAR LA BASE DE DATOS.
+
             consulta_cursor = ingresar_gastos.cursor()
+
+            # ENTRADAS DE USUARIO PARA INGRESAR EL GASTO E IDENTIFICAR AL USUARIO.
+
             ingresar_gasto = str(input("Ingresa el gasto: "))
             nombre_usuario = str(input("Ingresa tu nombre de registro: "))
             apellido_usuario = str(input("Ingresa tu apellido de registro: "))
+
+            # CONSULTA SQL PARA IDENTIFICAR EL ID DEL USUARIO Y CON ESTE SU NOMBRE Y APELLIDO.
+
             consulta_cursor.execute("SELECT usuario_ID FROM usuario WHERE nombre_usuario = ? AND apellido_usuario = ? ",(nombre_usuario, apellido_usuario))
+
+            # CON EL METODO "fetchone" SE BUSCA EL REGISTRO DE LA CLAVE.
+
             usuario = consulta_cursor.fetchone()
+
             if usuario:
+
+                #  RASTREAR AL CLIENTE EN LA BASE DE DATOS.
+
                 usuario_id = usuario[0]
             else:
                 print("Usuario no encontrado.")
+
+            # CONSULTA SQL PARA INGRESAR EL GASTO DEL USUARIO Y SU NOMBRE
+
             consulta_cursor.execute("INSERT INTO gastos (gasto_usuario, usuario_ID) VALUES (?,?)",(ingresar_gasto, usuario_id))
+
+            # SE SUBEN LOS CAMBIOS A LA BASE DE DATOS.
+
             ingresar_gastos.commit()
+
+            # MESNAJE DE EXITO.
+
             print("Gasto ingresado.")
+
+    # MANEJO DE ERROES.
+    
     except ValueError:
         print("Error de digitacion, volver a intentar.")
     except sqlite3.Error as error:
